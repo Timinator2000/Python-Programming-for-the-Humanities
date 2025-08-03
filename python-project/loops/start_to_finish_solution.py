515753e1be60a73f48532cf1cddb87f032098048
Timinator <74882103+Timinator2000@users.noreply.github.com>
2025-08-03 17:31:04 -0600

    Delete python-project/loops directory

--- python-project/loops/start_to_finish_solution.py
+++
@@ -1,9 +0,0 @@
-def start_to_finish(start: int, finish: int) -> None:
-    for i in range(start, finish + 1):
-        print(i)
-
-
-# alternate solution using str.join()
-def start_to_finish_alt(start: int, finish: int) -> None:
-    if finish >= start:
-        print('\n'.join(str(i) for i in range(start, finish + 1)))
